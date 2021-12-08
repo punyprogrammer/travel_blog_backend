46965e683869d4374bac2a823bb5376e0a127c47
Amardeep Ganguly <you@example.com>
2021-12-09 00:25:57 +0530

    Adding headers

--- index.js
+++ index.js
@@ -34,7 +34,14 @@ app.post("/api/upload", upload.single("file"), (req, res) => {
 });
 
 //middleware
-
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "https://amar-blog.herokuapp.com"); // update to match the domain you will make the request from
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 app.use(express.json());
 app.use(cors());
 app.use("/images", express.static(path.join(__dirname, "/images")));
